7553d220209cfd4271dbf814c6195406d7cc15b2
kahlua <lucasclaude3@gmail.com>
2019-02-27 13:18:36 +0100

    Updates

---
+++ precache-manifest.74f0a154bb2559d421fdfd0e38b4a80f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "79f6b6a0c4f3cca321dc458fe7e6544a",
+    "url": "/blindtest-spotify/static/media/logo.79f6b6a0.svg"
+  },
+  {
+    "revision": "9d4cbf40dcdac09bda6fa0aff397246b",
+    "url": "/blindtest-spotify/static/media/loading.9d4cbf40.svg"
+  },
+  {
+    "revision": "2bed69904f22342c89d6",
+    "url": "/blindtest-spotify/static/js/runtime~main.2bed6990.js"
+  },
+  {
+    "revision": "18d2f2dba45aea62610d",
+    "url": "/blindtest-spotify/static/js/main.18d2f2db.chunk.js"
+  },
+  {
+    "revision": "1b0b86136ef490fc9962",
+    "url": "/blindtest-spotify/static/js/2.1b0b8613.chunk.js"
+  },
+  {
+    "revision": "18d2f2dba45aea62610d",
+    "url": "/blindtest-spotify/static/css/main.bae45d32.chunk.css"
+  },
+  {
+    "revision": "7d8f218e4f4b85e88c422f9cd1620f4a",
+    "url": "/blindtest-spotify/index.html"
+  }
+];
